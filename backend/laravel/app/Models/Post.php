285795ea2ac441f3770ac9e2a6a9f2237123fb88
Aurelia Saskia <aurelia.saskia9@gmail.com>
2025-11-30 16:11:13 +0000

    Implement basic seeders for dummy data & update README

--- backend/laravel/app/Models/Post.php
+++ backend/laravel/app/Models/Post.php
@@ -18,7 +18,7 @@ class Post extends Model
 
     public function category()
     {
-        return $this->belongTo(Category::class);
+        return $this->belongsTo(Category::class);
     }
 
     public function user()
--- backend/laravel/app/Models/User.php
+++ backend/laravel/app/Models/User.php
@@ -9,38 +9,33 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // The attributes that are mass assignable.
+    // @var list<string>
+
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'dob',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    // The attributes that should be hidden for serialization.
+    // @var list<string>
+
     protected $hidden = [
         'password',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    // Get the attributes that should be cast.
+    // @return array<string, string>
+
     protected function casts(): array
     {
         return [
             'password' => 'hashed',
+            'dob' => 'date',
         ];
     }
 
